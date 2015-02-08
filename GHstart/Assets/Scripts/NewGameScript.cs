80f083356943218adb99f95aa984ec4ab0ec1dd6
Core <seorisachs@gmail.com>
2015-02-08 18:06:14 -0500

    debug.log of text

---
+++ GHstart/Assets/Scripts/NewGameScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGameScript : MonoBehaviour {
+
+	void OnMouseOver(){
+
+		Debug.Log("works");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
