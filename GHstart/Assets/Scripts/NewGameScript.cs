b3369653e73e1c424cd44a4caa3f92f0974be38d
Core <seorisachs@gmail.com>
2015-02-10 12:28:17 -0500

    audio test chime
    works, sounds like shit though

--- GHstart/Assets/Scripts/NewGameScript.cs
+++ GHstart/Assets/Scripts/NewGameScript.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class NewGameScript : MonoBehaviour {
 
+	public AudioClip testchime; 
+
 	void OnMouseOver(){
 
 		Debug.Log("works");
+		audio.PlayOneShot(testchime);
+		audio.Play ();
 	}
 
 	// Use this for initialization
