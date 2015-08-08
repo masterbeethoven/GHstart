438518b0dcf7a49a6340dc5825ccb65f7d1ed94a
Core <seorisachs@gmail.com>
2015-08-08 15:09:21 -0400

    basic fps

---
+++ GHstart/Assets/Scripts/FPSScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSScript : MonoBehaviour {
+
+	public float speed = 1.5f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.position+=Vector3.right*speed *Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.position+=Vector3.left*speed *Time.deltaTime; 
+		}
+
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.position+=Vector3.back*speed*Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.S))
+		{
+			transform.position+=Vector3.forward*speed*Time.deltaTime;
+		}
+
+
+	
+	}
+}
