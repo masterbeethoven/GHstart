dfb75f6f718a8557c6f454852fb942ef5a940fdc
Core <seorisachs@gmail.com>
2015-09-09 21:14:32 -0400

    sound assets

---
+++ GHstart/Assets/Scripts/AudioMouseOverScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioMouseOverScript : MonoBehaviour {
+
+	public AudioClip test;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter(){
+		Debug.Log("works");
+		GetComponent<AudioSource>().PlayOneShot(test);
+		GetComponent<AudioSource>().Play ();
+
+
+	}
+}
--- GHstart/Assets/Scripts/RayCastScript.cs
+++ GHstart/Assets/Scripts/RayCastScript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class RayCastScript : MonoBehaviour {
 
+
+	RaycastHit hit;
+	Ray ray; 
 	// Use this for initialization
 	void Start () {
 	
@@ -10,11 +13,16 @@ public class RayCastScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		RaycastHit hit;
-		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit))
+		{
+			print (hit.collider.name);
+		}
+		/*Vector3 fwd = transform.TransformDirection(Vector3.forward);
 		if (Physics.Raycast(transform.position, fwd, out hit, 10))
 			if(hit.collider !=null)
-			Debug.Log(hit.collider.gameObject.name);
+			Debug.Log(hit.collider.gameObject.name);*/
 				
 			//print ("front");
 	}
