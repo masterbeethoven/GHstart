4a7fc04581b559d4d3b8e65934db969ac880ca56
Core <seorisachs@gmail.com>
2015-10-24 02:21:45 -0700

    took out glitch, invisi cube around loveseat

--- GHstart/Assets/Scripts/CaneContactScript.cs
+++ GHstart/Assets/Scripts/CaneContactScript.cs
@@ -2,36 +2,45 @@
 using System.Collections;
 
 public class CaneContactScript : MonoBehaviour {
-
+	
+	private const float delay = 2; // Delay between playing sound
+	
 	public AudioClip clip;
 	AudioSource sound;
-
+	private float timeOfLastPlay;
 
 
 	// Use this for initialization
 	void Start () {
 
-	
+		sound = GetComponent<AudioSource>();
+		timeOfLastPlay = -1 * delay;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 	
 	}
 
 	void OnCollisionEnter(){
-		//Debug.Log ("bang");
-		GetComponent<AudioSource>().Play(); //VO of object
-	
 
-		/*sound=GetComponent<AudioSource>();
-		if (!sound.isPlaying){
-			sound.clip = clip;
-			sound.Play();*/
-			sound=GetComponent<AudioSource>();// sfx of collision
+		float currentTime = Time.time;
+		if ((currentTime - timeOfLastPlay) > delay) {
+			//Debug.Log ("bang");
+			sound.Play();
 			sound.PlayOneShot(clip);
-
-
+			timeOfLastPlay = currentTime;
 		}
+
 	}
+
+//	void OnCollisionExit(){
+//		sound.Stop();
+//	}
+
+}
+
+
 
