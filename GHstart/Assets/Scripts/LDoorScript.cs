e3ca73272829f3dfb9bef7fcd7f307e09088d1e5
Core <seorisachs@gmail.com>
2015-11-07 16:46:46 -0500

    Basic Door anims

--- GHstart/Assets/Scripts/LDoorScript.cs
+++ GHstart/Assets/Scripts/LDoorScript.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class LDoorScript : MonoBehaviour {
 	bool open;
 	//bool open; open starts out as false. when do first tab, turns true. when true, -Vector3.  
-	public float doorValue = 5000f;
+	//public float doorValue = 5000f;
 
 	public AudioClip doorOpen;
 	public AudioClip doorClose;
+	public Animation LDoorAnims;
 	//AudioSource source; 
 
 	// Use this for initialization
@@ -21,7 +22,12 @@ public class LDoorScript : MonoBehaviour {
 		if (open == false){
 			if(Input.GetKeyDown(KeyCode.Tab)){
 				Debug.Log("tab");
-				transform.Rotate(Vector3.up *doorValue* Time.deltaTime);
+				transform.GetComponent<Animation>()["LDoorAnim"].normalizedTime = 0.0f;
+				transform.GetComponent<Animation>()["LDoorAnim"].speed = 1.0f;
+				transform.GetComponent<Animation>().CrossFade("LDoorAnim");
+				//transform.Rotate(Vector3.up *doorValue* Time.deltaTime);
+				GetComponent<Animation>().Play("LDoorAnim");
+
 				open=true;	
 					if (open ==true){
 						//GetComponent<AudioSource>().Play();
@@ -33,11 +39,16 @@ public class LDoorScript : MonoBehaviour {
 			if(Input.GetKeyDown(KeyCode.Tab)){
 				Debug.Log("tab");
 				//transform.Rotate(-Vector3.up * doorValue* Time.deltaTime);
-				transform.Rotate(Vector3.down * doorValue* Time.deltaTime);
+				//transform.Rotate(Vector3.down * doorValue* Time.deltaTime);
+				transform.GetComponent<Animation>()["LDoorAnim"].normalizedTime = 1.0f;
+				transform.GetComponent<Animation>()["LDoorAnim"].speed = -1.0f;
+				transform.GetComponent<Animation>().CrossFade("LDoorAnim");
+				//GetComponent<Animation>().Play("CloseLDoorAnim");
 				open=false;
-				if(open==false){
+					if(open==false){
+					//yield WaitForSeconds(1f);
 					GetComponent<AudioSource>().PlayOneShot(doorClose);
-				}
+					}
 			}
 		}
 	
---
+++ GHstart/Assets/Scripts/RDoorScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RDoorScript : MonoBehaviour {
+	bool open;
+	//bool open; open starts out as false. when do first tab, turns true. when true, -Vector3.  
+	//public float doorValue = 5000f;
+	
+	public AudioClip doorOpen;
+	public AudioClip doorClose;
+
+	//AudioSource source; 
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open == false){
+			if(Input.GetKeyDown(KeyCode.Tab)){
+				Debug.Log("tab");
+
+				transform.GetComponent<Animation>()["RDoorAnim"].normalizedTime = 1.0f;
+				transform.GetComponent<Animation>()["RDoorAnim"].speed = -1.0f;
+				transform.GetComponent<Animation>().CrossFade("RDoorAnim");
+				//anim was backwards so i'll just deal with it this way
+				open=true;	
+				if (open ==true){
+					GetComponent<AudioSource>().PlayOneShot(doorOpen);
+				}
+			}
+		}
+		else{
+			if(Input.GetKeyDown(KeyCode.Tab)){
+				Debug.Log("tab");
+
+				transform.GetComponent<Animation>()["RDoorAnim"].normalizedTime = 0.0f;
+				transform.GetComponent<Animation>()["RDoorAnim"].speed = 1.0f;
+				transform.GetComponent<Animation>().CrossFade("RDoorAnim");
+				GetComponent<Animation>().Play("RDoorAnim");
+				open=false;
+				if(open==false){
+					GetComponent<AudioSource>().PlayOneShot(doorClose);
+				}
+			}
+		}
+		
+		
+		
+		
+	}
+}
+
+
